ed6e468784065ba9e71be89de82f4363a34a51cb
ANAND8439 <anandselvaraj2004@gmail.com>
2024-07-09 14:38:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('Night').addEventListener('click',function(){
+    document.getElementById("image").src= "https://sph.umich.edu/pursuit/2020posts/2020images/Sleep101.jpg"
+});
+document.getElementById('Day').addEventListener('click',function(){
+    document.getElementById("image").src= "https://lh3.googleusercontent.com/proxy/bKVmV9pqSQizSqwtb7O1i_69-no2x1b_nBGfzHvSL_8qr829gb83sCH7mX885WtaqHTbLJtvN-a33qIynCOnNirPF3QAgIqCCIJEFrVY_3xD1M1Xf5E33OMirJCo0Dmj3L9pR4VAmLV1H70g5w" 
+})
